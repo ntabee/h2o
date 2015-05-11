2e238e2b9127ce9d0cfdb610fba3033a4e9a2ad7
ntabee <ntabee@gmail.com>
2015-05-12 00:44:00 +0900

    Fix run.sh

--- wrk/run.sh
+++ wrk/run.sh
@@ -6,6 +6,6 @@ if [ "$1" = "" ]; then
     exit 1
 fi
 
-./gen-paths.sh "$1" > paths.txt
+./gen-paths.sh > paths.txt
 
-wrk -c 1000 -t 4 -d 30s -s traverse.lua http://localhost:8080/
+wrk -c 1000 -t 4 -d 30s -s traverse.lua "$1"
